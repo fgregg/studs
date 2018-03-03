c3c9f199208ef3b8c6e710d4327a312b4fbb7f4c
Forest Gregg <fgregg@uchicago.edu>
2018-03-03 13:01:25 -0600

    fixed up author and published dates

--- terkel.py
+++ terkel.py
@@ -1,5 +1,9 @@
+import calendar
+
 import requests
 import feedgen.feed
+import dateutil
+import pytz
 
 def all_entries(base_url, increment):
     start = 0
@@ -21,8 +25,8 @@ fg = feedgen.feed.FeedGenerator()
 fg.load_extension('podcast')
 fg.link(href='http://example.com', rel='alternate' )
 fg.title('The Studs Terkel Radio Archive')
-fg.author( {'name':'John Doe','email':'john@example.de'} )
-fg.description('foo')
+fg.podcast.itunes_author('Studs Terkel')
+fg.description('The WFMT Radio Network, along with the Studs Terkel Center for Oral History of the Chicago History Museum and many other committed partners, are working toward turning this site into the first-ever, comprehensive online Studs Terkel Radio Archive.')
 
 query = 'https://studsterkel.digitalrelab.com/lwm/ajax.php?operation=text-search&txt=&limit={},{}&sort=created_DESC'
 
@@ -44,7 +48,19 @@ for entry in all_entries(query, 25):
         fe.enclosure(url, entry['size'], 'audio/mpeg')
         fe.podcast.itunes_duration(duration)
         fe.updated(entry['modified'])
-        fe.published(entry['created'])
+        created_datetime = dateutil.parser.parse(entry['created'][0])
+        try:
+            original_broadcast = dateutil.parser.parse(entry['archival_broadcast_date'][0],
+                                                       default=created_datetime)
+        except (KeyError, ValueError):
+            try:
+                original_broadcast = dateutil.parser.parse(entry['wfmt_date_broadcast'][0],
+                                                           default=created_datetime)
+            except (calendar.IllegalMonthError, KeyError):
+                original_broadcast = created_datetime
+            
+        original_broadcast = pytz.utc.localize(original_broadcast)
+        fe.published(original_broadcast)
         try:
             fe.rss_entry()
         except:
